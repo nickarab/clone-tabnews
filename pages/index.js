7acba944b19b640c84de88af15d0ddc28613dd97
Manoel Barbosa <ManoelBDDutra@outlook.com>
2026-05-27 21:59:06 -0300

    page under construction

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,20 @@
-function Home() {
-  return <h1>Rita, eu amo você. Se você me ama, da uma risadinha!</h1>;
-}
+import React from 'react';
+import styles from '../archive/styles/animation.module.css';
 
-export default Home;
+export default function UnderConstruction() {
+  return (
+    <div className={styles.container}>
+      {/* 1ª Camada: Fundo */}
+      <div className={styles.background}></div>
+      
+      {/* 2ª Camada: Sonic */}
+      <div className={styles.sonic}></div>
+
+      {/* 3ª Camada: Texto na frente de tudo */}
+      <div className={styles.textoContainer}>
+        <h1 className={styles.titulo}>PÁGINA EM CONSTRUÇÃO</h1>
+        <p className={styles.subtitulo}>Construindo algo incrível na velocidade da luz</p>
+      </div>
+    </div>
+  );
+}
