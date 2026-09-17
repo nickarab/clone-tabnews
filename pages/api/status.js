8290b0d6be65efa055b337ba8a5d875150d3cf5e
Manoel Barbosa <ManoelBDDutra@outlook.com>
2026-09-16 21:28:27 -0300

    stateless database

--- pages/api/status.js
+++
@@ -1,5 +0,0 @@
-function status(request, response) {
-  response.status(200).json({ "eu to sem tempo": "mas vou terminar essa joça" });
-}
-
-export default status;
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
+import databse from '../../../../infra/database.js'
+
+
+async function status(request, response) {
+  const result = await databse("select 1+1;");
   response.status(200).json({ "eu to sem tempo": "mas vou terminar essa joça" });
+  console.log(result)
 }
 
 export default status;
